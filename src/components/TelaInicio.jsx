dad9402a5d99134a7eebc84148645eef01ee7951
Isadora-R <d2022015952@unifei.edu.br>
2023-09-30 17:54:03 -0300

    att

--- src/components/TelaInicio.jsx
+++ src/components/TelaInicio.jsx
@@ -1,14 +1,33 @@
-import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native'
 import React from 'react'
 import styles from './Styles'
 
 export default function TelaInicio() {
   return (
-    <ScrollView style={styles.todasTelas}>
-      <Text>Tela do inicio</Text>
-    </ScrollView>
+    <View style={estilo.topo}>
+      <Image style={estilo.imagem} source={require('../assets/logoSemText.png')} />
+      {/* Logo ficar fixa no topo */}
+      <ScrollView style={styles.todasTelas}>
+        <Text>Tela do inicio</Text>
+      </ScrollView>
+    </View >
   )
 }
+
+estilo = StyleSheet.create(
+  {
+    topo: {
+      backgroundColor: '#ffae15',
+    },
+    imagem: {
+      resizeMode: 'cover',
+      alignSelf: 'center',
+      width: 85,
+      height: 60,
+
+    }
+  }
+);
 
 
 /*
